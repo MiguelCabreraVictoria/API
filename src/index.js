5aad95d6d9de322bffda489951ca07166275ba10
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-01-02 12:13:40 -0600

    fix: port number 3000 -> 8000

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { testRedisConnection } from './cache'
 import app from './app';
 import './database';
 
-app.listen(3000, () => {    
+app.listen(8000, () => {    
   testRedisConnection();
   console.log('Server listening on port 8000');
 });
